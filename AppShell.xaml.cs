cfc73068039b53961409475b841c42a300d8145c
DESKTOP-AOQ6DJH\olvin <olvinrool@gmail.com>
2023-11-17 01:52:41 -0600

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Tarea_2_4
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Maui.Controls;
+
+using System;
+using System.IO;
+
+namespace Tarea2_4
+{
+    public partial class MainPage : ContentPage
+    {
+        private string videoPath;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnRecordVideoClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var options = new MediaCaptureVideoOptions
+                {
+                    MaximumRecordingTime = TimeSpan.FromSeconds(30),
+                };
+
+                var result = await MediaPicker.CaptureVideoAsync(options);
+
+                if (result != null)
+                {
+                    videoPath = result.FullPath;
+                    videoView.Source = videoPath;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejar excepciones
+            }
+        }
+    }
+}
+
+    internal class MediaCaptureVideoOptions : MediaPickerOptions
+    {
+        public TimeSpan MaximumRecordingTime { get; set; }
+    }
+}
